73944ac5f80873f34c50d97edc840620d3b8c2cf
robFraser1111 <30624101+robFraser1111@users.noreply.github.com>
2026-08-30 18:49:31 +1000

    Add Sentry error reporting for unhandled crashes. (#1)

--- src/GranTurismoTelemetry/Program.cs
+++ src/GranTurismoTelemetry/Program.cs
@@ -1,18 +1,59 @@
 using Avalonia;
+using Sentry;
 using System;
+using System.Linq;
 
 namespace GranTurismoTelemetry;
 
 sealed class Program
 {
+    internal const string SentryDsn =
+        "https://801b3f05d4f4e7eb0353671292edaf3d@o4511995844231168.ingest.us.sentry.io/4511997531848704";
+
     [STAThread]
     public static void Main(string[] args)
     {
+        using var _ = SentrySdk.Init(options =>
+        {
+            options.Dsn = Environment.GetEnvironmentVariable("SENTRY_DSN") ?? SentryDsn;
+            options.IsGlobalModeEnabled = true;
+            options.SendDefaultPii = true;
+            options.TracesSampleRate = 1.0;
+            options.Release = "gran-telemetry@" +
+                (typeof(Program).Assembly.GetName().Version?.ToString(3) ?? "0.9.0");
+#if DEBUG
+            options.Debug = true;
+            options.Environment = "development";
+#else
+            options.Environment = "production";
+#endif
+            options.SetBeforeSend((evt, _) =>
+            {
+                evt.ServerName = null;
+                return evt;
+            });
+        });
+
         AppDomain.CurrentDomain.UnhandledException += (_, e) =>
         {
             if (e.ExceptionObject is Exception ex)
                 CrashDialog.Show(ex);
         };
+
+        if (args.Any(a => string.Equals(a, "--sentry-test", StringComparison.OrdinalIgnoreCase)))
+        {
+            try
+            {
+                throw new InvalidOperationException(
+                    "Sentry test error " + DateTime.UtcNow.ToString("o"));
+            }
+            catch (Exception ex)
+            {
+                CrashDialog.Show(ex);
+            }
+
+            return;
+        }
 
         try
         {
